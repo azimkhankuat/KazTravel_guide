864aff5976c675a314306a2ea28d3a04c555ebea
azimkhankuat <kuat.0111@gmail.com>
2021-04-27 22:57:07 +0500

    Committed 27.04.21 - Tours page updated

--- main/models.py
+++ main/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 class Tour(models.Model):
     title = models.TextField('Title of the tour')
+    type = models.TextField('Type of the tour')
     desc = models.TextField('Description of the tour')
+    price = models.IntegerField('Price of the tour')
+    duration = models.TextField('Duration of the tour')
+    price_include = models.TextField('Prices include')
+    image = models.TextField('Image link')
 
     def __str__(self):
         return self.title
