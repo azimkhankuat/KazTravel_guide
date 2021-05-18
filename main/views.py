28de29342be95e73cb3d101d12f623ca07fc540e
azimkhankuat <kuat.0111@gmail.com>
2021-05-18 22:45:51 +0500

    Committed 18.05.2021 - Cart added

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views import View
 from django.views.generic import TemplateView
 
-from main.models import KazTour, Category
+from main.models import *
 
 
 def index(request):
@@ -34,6 +35,94 @@ class TourDetailView(TemplateView):
         product.save()
         context['product'] = product
         return context
+
+
+class AddToCartView(TemplateView):
+    template_name = "main/addtocart.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get product id from requested url
+        product_id = self.kwargs['pro_id']
+        # get product
+        product_obj = KazTour.objects.get(id=product_id)
+
+        # check if cart exists
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            cart_obj = TourCart.objects.get(id=cart_id)
+            this_product_in_cart = cart_obj.tourcartproduct_set.filter(
+                product=product_obj)
+
+            # item already exists in cart
+            if this_product_in_cart.exists():
+                cartproduct = this_product_in_cart.last()
+                cartproduct.quantity += 1
+                cartproduct.subtotal += product_obj.price
+                cartproduct.save()
+                cart_obj.total += product_obj.price
+                cart_obj.save()
+            # new item is added in cart
+            else:
+                cartproduct = TourCartProduct.objects.create(
+                    cart=cart_obj, product=product_obj, rate=product_obj.price, quantity=1, subtotal=product_obj.price)
+                cart_obj.total += product_obj.price
+                cart_obj.save()
+
+        else:
+            cart_obj = TourCart.objects.create(total=0)
+            self.request.session['cart_id'] = cart_obj.id
+            cartproduct = TourCartProduct.objects.create(
+                cart=cart_obj, product=product_obj, rate=product_obj.price, quantity=1, subtotal=product_obj.price)
+            cart_obj.total += product_obj.price
+            cart_obj.save()
+
+        return context
+
+
+class MyCartView(TemplateView):
+    template_name = "main/mycart.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            cart = TourCart.objects.get(id=cart_id)
+        else:
+            cart = None
+        context['cart'] = cart
+        return context
+
+
+class ManageCartView(View):
+    def get(self, request, *args, **kwargs):
+        cp_id = self.kwargs["cp_id"]
+        action = request.GET.get("action")
+        cp_obj = TourCartProduct.objects.get(id=cp_id)
+        cart_obj = cp_obj.cart
+
+        if action == "inc":
+            cp_obj.quantity += 1
+            cp_obj.subtotal += cp_obj.rate
+            cp_obj.save()
+            cart_obj.total += cp_obj.rate
+            cart_obj.save()
+        elif action == "dcr":
+            cp_obj.quantity -= 1
+            cp_obj.subtotal -= cp_obj.rate
+            cp_obj.save()
+            cart_obj.total -= cp_obj.rate
+            cart_obj.save()
+            if cp_obj.quantity == 0:
+                cp_obj.delete()
+
+        elif action == "rmv":
+            cart_obj.total -= cp_obj.subtotal
+            cart_obj.save()
+            cp_obj.delete()
+        else:
+            pass
+        return redirect("mycart")
 
 
 def profile(request):
